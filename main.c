1fbb529a6ee7dfd0cd183cd69929444fe81a2ede
vintwon <vintwon@proton.me>
2026-03-15 15:03:37 -0400

    initial password adding

--- main.c
+++ main.c
@@ -45,17 +45,36 @@ void askNewPassphrase() {
     sodium_memzero(passPhrase, sizeof(passPhrase));
 }
 
+
+
+void addNewUser(char program[], char username[], char password[]){
+    fptr2 = fopen("data.1fuc", "a");
+    fprintf(fptr2, "\n%s{", program);
+    fprintf(fptr2,"\n#%s;", username);
+    fprintf(fptr2, "\n%s;", password);
+    fprintf(fptr2, "\n}");
+    fclose(fptr2);
+    printf("\n ADDED NEW USER\n");
+}
+
+
+
+
+
+
+
+
 int main(){ 
 
 
-    bool authenticated = false;
+    bool authenticated = true;
 
     if (sodium_init() < 0) {
         printf("Sodium failed init\n");
         return 1;
     }
 
-    bool newToProgram = false;//false by default
+    bool newToProgram = true;//false by default
     char pwd[256];
     char dataInFirstLn[crypto_pwhash_STRBYTES];
 
@@ -74,6 +93,9 @@ int main(){
         pwd[strcspn(pwd, "\n")] = 0;
 
         fptr = fopen("data.2fuc", "r");
+        if (fptr == NULL) {
+        printf("No master password set. Run first-time setup.\n");
+}
         fgets(dataInFirstLn, sizeof(dataInFirstLn), fptr);
 
         if (crypto_pwhash_str_verify(dataInFirstLn, pwd, strlen(pwd)) == 0) {
@@ -89,13 +111,39 @@ int main(){
 
 
     //##AUTHENTICATION ABOVE##//
+    char input[256];
+    char username[256];
+    char password[256];
+    while(authenticated == true){
+        printf("Enter an option: \n1. Read a password/program \n2. Add a new password/program\n3. Quit\n");
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
 
-    
+        if(strcmp(input, "3") == 0){
+            authenticated = false;
+        }else if (strcmp(input, "2") == 0) {
+
+        printf("Enter program name e.g. Discord: \n");
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
+        printf("Enter program username e.g. Johny123: \n");
+        fgets(username, sizeof(username), stdin);
+        username[strcspn(username, "\n")] = 0;
+        printf("Enter Program password e.g. abc123password: \n");
+        fgets(password, sizeof(password), stdin);
+        password[strcspn(password, "\n")] = 0;
+
+        addNewUser(input, username, password);
+
+
+        }
+
+    }
 
 
 
 
 
-
+    printf("\nGoodbye!\n");
     return 0;
 }
