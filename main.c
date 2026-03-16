4f6632f3eba44e1ba5cdaea4f5c3ca1445f0e9e5
vintwon <vintwon@proton.me>
2026-03-16 19:20:24 -0400

    Password Reading Implemented

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sodium.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*compile command: gcc main.c \
   -Ivendor/libsodium/build/include \
@@ -13,6 +14,26 @@
 
 FILE *fptr;
 FILE *fptr2;
+
+int count_lines(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    int count = 0;
+    int ch;
+
+    if (fp == NULL) {
+        printf("Could not open file %s\n", filename);
+        return -1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '\n') {
+            count++;
+        }
+    }
+
+    fclose(fp);
+    return count;
+}
 
 void askNewPassphrase() {
     char passPhrase[256];
@@ -45,27 +66,20 @@ void askNewPassphrase() {
     sodium_memzero(passPhrase, sizeof(passPhrase));
 }
 
-
-
 void addNewUser(char program[], char username[], char password[]){
     fptr2 = fopen("data.1fuc", "a");
+
     fprintf(fptr2, "\n%s{", program);
-    fprintf(fptr2,"\n#%s;", username);
-    fprintf(fptr2, "\n%s;", password);
+    fprintf(fptr2,"\n#USERNAME:%s;", username);
+    fprintf(fptr2, "\n&PASSWORD:%s;", password);
     fprintf(fptr2, "\n}");
+
     fclose(fptr2);
-    printf("\n ADDED NEW USER\n");
+
+    printf("\nADDED NEW USER\n");
 }
 
-
-
-
-
-
-
-
 int main(){ 
-
 
     bool authenticated = true;
 
@@ -74,28 +88,38 @@ int main(){
         return 1;
     }
 
-    bool newToProgram = true;//false by default
+    bool newToProgram = false;//false by default
+
+
+
+
+
+
     char pwd[256];
     char dataInFirstLn[crypto_pwhash_STRBYTES];
 
-    //check if new to program and adjust the bool properly
-
-
     if(newToProgram == true){
+
         printf("Welcome to PassoutManager!\n");
         askNewPassphrase();
+
         fptr2 = fopen("data.1fuc", "w");
         fclose(fptr2);
+
     }else{
-        printf("Enter your master passphrase: \n");
+
+        printf("Enter your master passphrase:\n");
 
         fgets(pwd, sizeof(pwd), stdin);
         pwd[strcspn(pwd, "\n")] = 0;
 
         fptr = fopen("data.2fuc", "r");
+
         if (fptr == NULL) {
-        printf("No master password set. Run first-time setup.\n");
-}
+            printf("No master password set. Run first-time setup.\n");
+            return 1;
+        }
+
         fgets(dataInFirstLn, sizeof(dataInFirstLn), fptr);
 
         if (crypto_pwhash_str_verify(dataInFirstLn, pwd, strlen(pwd)) == 0) {
@@ -103,49 +127,145 @@ int main(){
             authenticated = true;
         } else {
             printf("Wrong password\n");
+            authenticated = false;
         }
 
         sodium_memzero(pwd, sizeof(pwd));
         fclose(fptr);
     }
 
-
-    //##AUTHENTICATION ABOVE##//
     char input[256];
     char username[256];
     char password[256];
+
     while(authenticated == true){
-        printf("Enter an option: \n1. Read a password/program \n2. Add a new password/program\n3. Quit\n");
+
+        printf("\nEnter an option:\n");
+        printf("1. Read a password/program\n");
+        printf("2. Add a new password/program\n");
+        printf("3. Quit\n");
+
         fgets(input, sizeof(input), stdin);
         input[strcspn(input, "\n")] = 0;
 
         if(strcmp(input, "3") == 0){
+
             authenticated = false;
+
         }else if (strcmp(input, "2") == 0) {
 
-        printf("Enter program name e.g. Discord: \n");
-        fgets(input, sizeof(input), stdin);
-        input[strcspn(input, "\n")] = 0;
-        printf("Enter program username e.g. Johny123: \n");
-        fgets(username, sizeof(username), stdin);
-        username[strcspn(username, "\n")] = 0;
-        printf("Enter Program password e.g. abc123password: \n");
-        fgets(password, sizeof(password), stdin);
-        password[strcspn(password, "\n")] = 0;
+            printf("Enter program name (e.g. Discord):\n");
+            fgets(input, sizeof(input), stdin);
+            input[strcspn(input, "\n")] = 0;
 
-        addNewUser(input, username, password);
+            printf("Enter program username:\n");
+            fgets(username, sizeof(username), stdin);
+            username[strcspn(username, "\n")] = 0;
 
+            printf("Enter program password:\n");
+            fgets(password, sizeof(password), stdin);
+            password[strcspn(password, "\n")] = 0;
 
+            addNewUser(input, username, password);
+
+        }else if (strcmp(input, "1") == 0){
+
+            fptr2 = fopen("data.1fuc", "r");
+
+            if(fptr2 != NULL){
+
+                char line[256];
+                char programs[100][256];
+                int programCount = 0;
+
+                while(fgets(line, sizeof(line), fptr2)){
+
+                    if(strchr(line, '{') != NULL){
+
+                        line[strcspn(line, "{")] = 0;
+
+                        strcpy(programs[programCount], line);
+                        programCount++;
+                    }
+                }
+
+                if(programCount == 0){
+                    printf("No saved programs.\n");
+                    fclose(fptr2);
+                    continue;
+                }
+
+                printf("\nSelect a program:\n");
+
+                for(int i = 0; i < programCount; i++){
+                    printf("%d. %s\n", i+1, programs[i]);
+                }
+
+                printf("Choice: ");
+                fgets(input, sizeof(input), stdin);
+                int choice = atoi(input);
+
+                if(choice < 1 || choice > programCount){
+                    printf("Invalid choice\n");
+                    fclose(fptr2);
+                    continue;
+                }
+
+                char chosenProgram[256];
+                strcpy(chosenProgram, programs[choice-1]);
+
+                rewind(fptr2);
+
+                bool insideBlock = false;
+
+                while(fgets(line, sizeof(line), fptr2)){
+
+                    if(strchr(line, '{') != NULL){
+
+                        char temp[256];
+                        strcpy(temp, line);
+
+                        temp[strcspn(temp, "{")] = 0;
+
+                        if(strcmp(temp, chosenProgram) == 0){
+                            insideBlock = true;
+                            continue;
+                        }
+                    }
+
+                    if(insideBlock){
+
+                        if(strncmp(line, "#USERNAME:", 10) == 0){
+
+                            char *token = strtok(line, ":");
+                            token = strtok(NULL, ";");
+
+                            printf("Username: %s\n", token);
+                        }
+
+                        if(strncmp(line, "&PASSWORD:", 10) == 0){
+
+                            char *token = strtok(line, ":");
+                            token = strtok(NULL, ";");
+
+                            printf("Password: %s\n", token);
+                        }
+
+                        if(strchr(line, '}') != NULL){
+                            break;
+                        }
+                    }
+                }
+
+                fclose(fptr2);
+
+            }else{
+
+                printf("file not found\n");
+            }
         }
-
     }
-
-
-
-
 
     printf("\nGoodbye!\n");
     return 0;
 }
-
-
